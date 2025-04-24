a300697020850a2ec5a738121e444742fdbc3c73
imtaki <161586687+imtaki@users.noreply.github.com>
2025-04-24 16:59:30 +0200

    navbar update

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,16 +1,38 @@
-import "../index.css"
+import "../index.css";
 
 export default function NavBar() {
+    const navItems = [
+        { id: 0, label: "Studies", href: "/studies" },
+        { id: 1, label: "Research", href: "/research" },
+        { id: 2, label: "About Boku", href: "/about" },
+    ];
+
+    const currentPath = window.location.pathname;
+
     return (
         <nav className="py-4">
             <div className="mx-auto max-h-screen px-4 flex justify-between items-center">
-                <span className="text-4xl font-bold text-green-700">Boku University</span>
+                <a href="/">
+                    <span className="text-4xl font-extrabold text-green-700">Boku University</span>
+                </a>
                 <ul className="flex space-x-6 text-gray-700 font-medium">
-                    <li><a href="/studies">Studies</a></li>
-                    <li><a href="/about">Research</a></li>
-                    <li><a href="/admissions">About</a></li>
-                    <li><a href="/departments">Boku</a></li>
-                    <li className="text-xl"><a href="/contact">Login </a></li>
+                    {navItems.map((item) => (
+                        <li key={item.id}>
+                            <a
+                                href={item.href}
+                                className={
+                                    currentPath === item.href
+                                      ? "text-green-700 font-semibold text-lg border-b-2 border-green-700 pb-1"
+                                      : "hover:text-green-700"
+                                  }
+                            >
+                                {item.label}
+                            </a>
+                        </li>
+                    ))}
+                    <li>
+                    <a className="text-xl font-bold" href="/login">Login</a>
+                    </li>
                 </ul>
             </div>
         </nav>
