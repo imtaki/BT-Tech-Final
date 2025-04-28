39412bb74dc6c48c6a29a60773545e245efe48ba
Martin <revoxgfx@gmail.com>
2025-04-28 17:32:44 +0200

    login page css fix

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -16,7 +16,7 @@ export default function Login() {
               id="email"
               name="email"
               type="email"
-              className="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-green-700 focus:outline-none focus:ring-1 focus:green-700"
+              className="w-full rounded-md border border-gray-300 px-3 py-2 focus:border-green-700 focus:outline-none focus:ring-1 focus:ring-green-700"
               placeholder="you@example.com"
               required
             />
